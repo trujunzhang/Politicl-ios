7bb978177bd066814ef68cc266891a91c06f3e68
trujunzhang <trujunzhang@gmail.com>
2016-08-12 10:10:14 +0800

    politicl for ios.

---
+++ PoliticlTests/PoliticlTests-Bridging-Header.h
@@ -0,0 +1,19 @@
+//
+//  WikipediaUnitTests-Bridging-Header.h
+//  Wikipedia
+//
+//  Created by Brian Gerstle on 7/7/15.
+//  Copyright (c) 2015 Wikimedia Foundation. All rights reserved.
+//
+
+//#import "WMFAsyncTestCase.h"
+//#import "WMFTestFixtureUtilities.h"
+//#import "MWKDataStore+TemporaryDataStore.h"
+//#import "WMFRandomFileUtilities.h"
+//#import "XCTestCase+MWKFixtures.h"
+//#import "WMFImageController+Testing.h"
+//#import "MWKSavedPageList+ImageMigrationTesting.h"
+//#import "WMFHTTPHangingProtocol.h"
+//#import "NSProcessInfo+WMFOperatingSystemVersionChecks.h"
+#import <Nocilla/Nocilla.h>
+#import <CocoaLumberjack/CocoaLumberjack.h>
