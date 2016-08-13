b2a74663c11e9c3a3b91926cdc04035e552fea12
trujunzhang <trujunzhang@gmail.com>
2016-08-13 10:25:52 +0800

    parse json by EVReflection.

--- PoliticlTests/AlamofireJsonToObjects/EVReflectionTests.swift
+++ PoliticlTests/AlamofireJsonToObjects/EVReflectionTests.swift
@@ -2,59 +2,61 @@ import Quick
 import Nimble
 import Foundation
 import SwiftyJSON
+import Mockingjay
 
 class EVReflectionSpec: QuickSpec {
     override func spec() {
         describe("Reflect the recent posts to an object instance") {
             
-            it("Fetching the AggregatedFeedContent object") {
-                let expectation = self.expectationWithDescription("Alamofire")
-                
-                PWPFeedFetcher.fetchingRecentPosts(1, completionHandler: {response in
-                    switch response{
-                    case .Success:
-                        let content = response.value
-                        let x = 0
-                        break
-                    case .Failure(let error):
-                        print(error)
+            beforeEach() {
+                if let path = NSBundle.mainBundle().pathForResource("get_recent_posts", ofType: "json") {
+                    do {
+                        let data = try NSData(contentsOfURL: NSURL(fileURLWithPath: path), options: NSDataReadingOptions.DataReadingMappedIfSafe)
+                        self.stub(http(.GET, uri: "http://www.politicl.com/api/get_recent_posts/?page=1"), builder: json(data))
+                    } catch let error as NSError {
+                        print(error.localizedDescription)
                     }
-                    expectation.fulfill()
-                })
-                self.waitForExpectationsWithTimeout(50.0, handler: nil)
+                } else {
+                    print("Invalid filename/path.")
+                }
             }
             
-            //            it("to the AggregatedFeedContent object") {
-            //
-            //                if let path = NSBundle.mainBundle().pathForResource("get_recent_posts", ofType: "json") {
-            //                    do {
-            //                        let data = try NSData(contentsOfURL: NSURL(fileURLWithPath: path), options: NSDataReadingOptions.DataReadingMappedIfSafe)
-            //                        var text = String(data: data, encoding: NSUTF8StringEncoding)
-            //                        let content = AggregatedFeedContent(json: JSON.parse(text!))
-            ////                        let posts = content.posts
-            //
-            //                        let x = 0
-            //
-            //                    } catch let error as NSError {
-            //                        print(error.localizedDescription)
-            //                    }
-            //                } else {
-            //                    print("Invalid filename/path.")
-            //                }
-            //            }
-            
-            //            it("fetch the recent posts"){
-            //
-            //                let URL = "http://www.politicl.com/api/get_recent_posts/?page=1"
-            //                Alamofire.request(.GET, URL)
-            //                    .responseObject { (response: Result<AggregatedFeedContent, NSError>) in
-            //                        if let result = response.value {
-            //                            // That was all... You now have a WeatherResponse object with data
-            //                        }
-            //                }
-            //
-            //            }
-            
         }
+        
+        it("Fetching the AggregatedFeedContent object") {
+            let expectation = self.expectationWithDescription("Alamofire")
+            
+            PWPFeedFetcher.fetchingRecentPosts(1, completionHandler: {response in
+                switch response{
+                case .Success:
+                    let content = response.value
+                    break
+                case .Failure(let error):
+                    print(error)
+                }
+                expectation.fulfill()
+            })
+            self.waitForExpectationsWithTimeout(50.0, handler: nil)
+        }
+        
+        //            it("to the AggregatedFeedContent object") {
+        //
+        //                if let path = NSBundle.mainBundle().pathForResource("get_recent_posts", ofType: "json") {
+        //                    do {
+        //                        let data = try NSData(contentsOfURL: NSURL(fileURLWithPath: path), options: NSDataReadingOptions.DataReadingMappedIfSafe)
+        //                        var text = String(data: data, encoding: NSUTF8StringEncoding)
+        //                        let content = AggregatedFeedContent(json: JSON.parse(text!))
+        ////                        let posts = content.posts
+        //
+        //                        let x = 0
+        //
+        //                    } catch let error as NSError {
+        //                        print(error.localizedDescription)
+        //                    }
+        //                } else {
+        //                    print("Invalid filename/path.")
+        //                }
+        //            }
+        
     }
 }
